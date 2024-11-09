706dc5090d504918863123f9af2334a1e25f72a4
qig <qig@com>
2024-11-09 17:22:38 +0800

    feature: 构建最小内核实现可引导启动的磁盘映像

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,21 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle] // 不重整函数名
+const UART0: *mut u8 = 0x09000000 as *mut u8; // ARM 架构上 UART 串行端口的内存地址
+
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let hello = b"Hello World!";
+    for &byte in hello {
+        unsafe {
+            *UART0 = byte; // 向 UART0 地址写入字节，通过串行端口输出
+        }
+    }
+
     loop {}
 }
 
-/// 函数将在 panic 时被调用
+/// 在发生 panic 时会调用该函数
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
